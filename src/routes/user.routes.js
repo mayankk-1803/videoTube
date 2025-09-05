89f34f858764b177e631bd4b53ae6e268e2812ea
Mayank Mathur <mayankmathur183@gmail.com>
2025-09-05 13:32:45 +0530

    register controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,16 @@
 import express, { Router } from 'express';
 import { registerUser } from '../controllers/user.controller.js';
+import { upload } from '../middlewares/multer.middleware.js';
 
 const userRouter = express.Router();
 
-userRouter.route('/register').post(registerUser)
+userRouter.route('/register').post( upload.fields([
+    {
+        name: 'avatar', maxCount: 1
+    },
+    {
+        name: 'coverImage', maxCount:1
+    }
+]),registerUser)
 
 export default userRouter;
